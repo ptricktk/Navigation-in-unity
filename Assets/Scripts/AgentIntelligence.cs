30d06ba9b441a23b6ec81b524aeb72b8bc92d886
Patrick Tak <lodzic@gmail.com>
2016-06-23 22:44:52 +0200

    Navigatie in Unity
    Demo met navigatie in Unity

---
+++ Assets/Scripts/AgentIntelligence.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentIntelligence : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+    GameObject[] NavigationPoints;
+
+    Vector3 TargetToReach;
+    int LastTargetReached;
+
+    // Use this for initialization
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        NavigationPoints = GameObject.FindGameObjectsWithTag("NavigationPoint");
+
+        TargetToReach = GetRandomDestination();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(TargetToReach);
+
+	}
+
+    Vector3 GetRandomDestination()
+    {
+        
+        int destination = Random.Range(0, NavigationPoints.Length);
+
+        // als nieuwe locatie gelijk is aan huidige, moet er een nieuwe locatie gekozen worden.
+        while (destination == LastTargetReached)
+        {
+            destination = Random.Range(0, NavigationPoints.Length);
+            Debug.Log("zelfde locatie afgevangen");
+        }
+        
+        LastTargetReached = destination;
+        return NavigationPoints[destination].transform.position;
+
+    }
+
+    public void SetNewDestination()
+    {
+        TargetToReach = GetRandomDestination();
+    }
+}
---
+++ Assets/Scripts/TriggerNewDestination.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerNewDestination : MonoBehaviour {
+
+	void OnTriggerEnter(Collider ObjectThatBumps)
+    {
+        ObjectThatBumps.GetComponent<AgentIntelligence>().SetNewDestination();
+        
+    }
+
+}
